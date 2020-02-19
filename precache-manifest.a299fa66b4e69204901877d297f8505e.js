49b4a80f53d6905bdb49636dc740ac21762c1fa3
Evan Mullins <evan.mullins@get10up.com>
2020-02-19 13:38:43 +0100

    Updates

---
+++ precache-manifest.a299fa66b4e69204901877d297f8505e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded652454620607e7e74c0494f763104",
+    "url": "/10up-whois/index.html"
+  },
+  {
+    "revision": "2108bc04a701f965cd72",
+    "url": "/10up-whois/static/css/main.4bef9d2e.chunk.css"
+  },
+  {
+    "revision": "6e564bbaf54a17cf35dc",
+    "url": "/10up-whois/static/js/2.70ea790a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/10up-whois/static/js/2.70ea790a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2108bc04a701f965cd72",
+    "url": "/10up-whois/static/js/main.108c9f20.chunk.js"
+  },
+  {
+    "revision": "643f07f23e504b2c4d78",
+    "url": "/10up-whois/static/js/runtime-main.0e8b8f79.js"
+  },
+  {
+    "revision": "a5fccafa8329ef0af8a011c1050c2627",
+    "url": "/10up-whois/static/media/10up-logo.a5fccafa.svg"
+  }
+]);
