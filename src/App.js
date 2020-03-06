bbb57a1e9f77a8c728a3a2ddbeeccc19feba8609
Evan Mullins <evan.mullins@get10up.com>
2020-03-06 13:32:32 +0100

    sort leaderboard on load, add selected user to app

--- src/App.js
+++ src/App.js
@@ -121,6 +121,7 @@ export default class App extends React.Component {
 		if ( this.state.leaderboard.length > 0 ) {
 			teamLeaderboard = this.state.leaderboard.filter((record) => record.teamName === teamName );
 		}
+		// sort leaderboard records by score
 		this.setState({ 
 			selectedTeam: this.state.groups.find(o => o.title === teamName ),
 			selectedLeaderboard: teamLeaderboard,
@@ -155,7 +156,8 @@ export default class App extends React.Component {
 		fbLeaderboardRef.on('value', (snapshot) =>{
 			let leaderboard = snapshot.val();
 			let leaderboard_ar = [];
-			// convert to array
+			
+			// convert to array - firebase tends to return as object
 			if ( !Array.isArray(leaderboard) ) {
 				leaderboard_ar = Object.keys(leaderboard).map(function(key, i) {
 					return leaderboard[key];
@@ -163,6 +165,13 @@ export default class App extends React.Component {
 			} else {
 				leaderboard_ar = leaderboard;
 			}
+
+			//sort records by speed/duration
+			leaderboard_ar.sort((a,b) => 
+				(parseInt(a.duration) > parseInt(b.duration)) ? 1 :
+				((parseInt(b.duration) > parseInt(a.duration)) ? -1 :
+				0)
+			);
 
 			this.setState({
 				leaderboard: leaderboard_ar,
@@ -202,6 +211,13 @@ export default class App extends React.Component {
 					<a class="code-link" href="https://github.com/circlecube/10up-whois">
 						<svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
 					</a>
+
+					<div title="Current User" className="current-user person-card" data-clicked="true" data-correct="true">
+						<img src={this.state.selectedAvatar} alt={this.state.selectedName}/>
+						<p className="detail">
+							<span className="person-name">{ this.state.selectedName }</span>
+						</p>
+					</div>
 
 					{ this.state.message &&
 						<div className="message" dangerouslySetInnerHTML={{__html: this.state.message}}></div>
